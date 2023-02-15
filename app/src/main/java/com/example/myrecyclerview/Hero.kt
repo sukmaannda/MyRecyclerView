5c72376040b3294d96bbf77ed2726cc43a5332e7
sukmaannda <p.sukmaananda@gmail.com>
2023-02-15 07:42:38 +0700

    first commit

---
+++ app/src/main/java/com/example/myrecyclerview/Hero.kt
@@ -0,0 +1,9 @@
+package com.example.myrecyclerview
+
+import android.provider.ContactsContract.DisplayPhoto
+
+data class Hero(
+    var name: String = "",
+    var from: String = "",
+    var photo: String = ""
+)
